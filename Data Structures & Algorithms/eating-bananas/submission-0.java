51f44bde6bc2923d8a8066caa2838f62d227912c
Nikita Martin <166871190+Kita-m17@users.noreply.github.com>
2026-07-19 20:17:08 +0200

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int max = piles[piles.length -1];
+
+        int l = 0, r = max, res = r;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            int hours = 0;
+            for(int i = 0; i<piles.length;i++){
+                hours+=Math.ceil((double)piles[i]/mid);
+                if(hours > h){
+                    break;
+                }
+            }
+
+            if(hours <= h){
+                r = mid-1;
+                res = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        return res;
+
+    }
+}
