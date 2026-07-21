1d4ff9f39eb0d9f273f1f5fb43f7bd6cba350a01
Nikita Martin <166871190+Kita-m17@users.noreply.github.com>
2026-07-21 20:54:31 +0200

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] res = new int[nums.length - k + 1];
+        List<Integer> vals = new ArrayList<>();
+
+        for(int i = 0; i<nums.length; i++){
+            vals.add(nums[i]);
+            if(vals.size() == k){
+                res[i-k+1] = Collections.max(vals);
+                vals.remove(0);
+
+            }
+
+        }
+        return res;
+    }
+}
