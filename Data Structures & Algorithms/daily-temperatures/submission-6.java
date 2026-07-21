4c38bf5726fe53d045a9a4809a1ffb1dfbb6ff54
Nikita Martin <166871190+Kita-m17@users.noreply.github.com>
2026-07-21 22:53:15 +0200

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int size = temperatures.length;
+        int[] res = new int[size];
+
+        for(int i = size -2; i>=0; i--){
+            int j = i+1;
+            while(j<size && temperatures[j] <= temperatures[i]){
+                if(res[j]==0){
+                    j = size;
+                    break;
+                }
+                j+=res[j];
+            }
+
+            if(j<size){
+                res[i] = j-i;
+            }else{
+                res[i] = 0;
+            };
+        }
+        return res;
+    }
+}
